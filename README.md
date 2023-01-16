edd902556913e3dd2bbad63b4dab88106298905e
Mutisostud02 <mutiso8395@gmail.com>
2023-01-16 15:47:33 +0300

    added README.md file with a view of how my project works and its limitations added index.html with all the necessary buttons and divs to function addes styles.css that styles our html button and divs on the page added script.js to add all the functions and interactivity to the buttons for the calculator to work

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # calculator
+We're building an online calculator.
+contains math functions that enable our calculator to perform simple maths such as addition, multiplication, subtraction and division.
---
+++ script.js
@@ -0,0 +1,61 @@
+const add = (a, b) => a += b;
+const multiply = (a, b) => a *= b;
+const divide = (a, b) => a /= b;
+const subtract = (a, b) => a -= b;
+
+function operate (operator, a, b){
+    if(operator === '+'){
+        return add(a,b);
+    }
+    else if(operator === '-'){
+        return subtract(a,b);
+    }else if(operator === '*'){
+        return multiply(a,b);
+    }
+    else if(operator === '/'){
+        return divide(a,b);
+    }
+}
+
+const digitButtons = document.querySelectorAll('.no');
+const operators = document.querySelectorAll('.ops');
+const display = document.querySelector('.display');
+const equals = document.querySelector('.equals');
+const clear = document.querySelector('.clear');
+
+let previousVal = '';
+let currentVal = '';
+let operand = '';
+
+//function to display numbers when numbers are clicked
+const varDisplay = digitButtons.forEach(function(number){
+    number.addEventListener('click',function (){
+        display.textContent += number.textContent;
+        currentVal = parseInt(display.innerText);
+    });
+   
+});
+
+
+
+operators.forEach(operators => {
+    operators.addEventListener('click', () => {
+previousVal = currentVal;
+currentVal = '';
+display.textContent = '';
+display.textContent += operators.textContent;
+operand = display.textContent;
+display.textContent = '';
+
+currentVal = parseInt(display.textContent);
+
+    })
+});
+
+equals.addEventListener('click', () => {
+    display.textContent = '';
+    display.textContent += operate(operand,previousVal,currentVal);
+   previousVal = currentVal;
+   currentVal = parseInt(display.innerText);
+    
+})
